e007fb5b6be2719d01ffe6424d7bfc8cb38b5284
Esp1ritus <antoshko.v@bk.ru>
2025-09-29 21:26:45 +0300

    Завёз базовый интерфейс, там можно печатать!

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #тут будет убийца ворда!
+#ура! я завёз интерфейс
+from tkinter import *
+
+window = Tk()
+window.geometry("300x300")
+window.title("wordk1ller")
+
+text = Text(window)
+text.pack()
+
+window.mainloop()
